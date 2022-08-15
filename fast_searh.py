af718b5d2d13b2d3807e0a3d13355f895f22460f
Barebore <borislavtarget@gmail.com>
2022-08-15 11:41:27 +0500

    1_1

---
+++ fast_searh.py
@@ -0,0 +1,22 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1,len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+            print('cycle #',i)
+            print(arr)
+            print(smallest,smallest_index)
+    return smallest_index
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+A = [5,3,6,2,10]
+
+print(selectionSort(A))
