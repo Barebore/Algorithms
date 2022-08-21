28156554e5b608734bb9742b812a4a39406ab87e
Barebore <borislavtarget@gmail.com>
2022-08-21 23:18:40 +0500

    123

---
+++ Any/notebook_area.py
@@ -0,0 +1,16 @@
+width = 40
+height = 55
+pages = 48
+print(width*height*pages)
+print(width*height*pages*25)
+a = width*height*pages*25
+
+width = 31
+height = 40
+pages = 96
+print(width*height*pages)
+print(width*height*pages*25)
+b = width*height*pages*25
+
+c = b / a
+print((c-1)*100)
