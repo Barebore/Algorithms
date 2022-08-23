2d371271433f9e8ced10e03af33a5e9b6be1be17
Barebore <borislavtarget@gmail.com>
2022-08-23 18:22:32 +0500

    123

--- Timofey_Khiryanov_lections/Lections/Lections_1.py
+++ Timofey_Khiryanov_lections/Lections/Lections_1.py
@@ -25,3 +25,25 @@ x = 16
 y = 3
 print(x//y) # деление с отбрасыванием остатка (5)
 print(x%y) # остаток от деления (3)
+print(-12%10)
+print(-12//10)
+print(-12%5)
+print(12%5)
+print(-12//5)
+# x = int(input())
+# while x > 0:
+#     y = x
+#     while y > 0:
+#         y -= 1
+#         print(y)
+#     x -= 1
+# print('stop')
+for x in 1,5,2,4,3:
+    print(x**2)
+print (0.2 + 0.7)
+print(2**100000)
+x = 2 ** 100000
+print(type(x))
+print(dir(x))
+2+2 == 4
+import this
