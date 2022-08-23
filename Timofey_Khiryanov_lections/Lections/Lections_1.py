b33d6a256e404b393caaf678f8dcfffe8f4a308b
Barebore <borislavtarget@gmail.com>
2022-08-23 16:26:15 +0500

    123

--- Timofey_Khiryanov_lections/Lections/Lections_1.py
+++ Timofey_Khiryanov_lections/Lections/Lections_1.py
@@ -1,5 +1,27 @@
+from re import A
+
+
 print()
 print('Hello, World')
 x = 'Hello, World'
 print(x)
 print(type(x))
+x = 1 + 2 + 3 * 2
+print(x)
+print(type(x))
+a = 2
+b = 5
+#tmp = a
+#a = b
+#b = tmp
+a,b = b,a # обмен переменныхз через временный кортеж из двух переменных (сковородки с блинами)
+print(a,b)
+print(a**b)
+print(a**b**b)
+a = -a
+print(a)
+print(2/3)
+x = 16
+y = 3
+print(x//y) # деление с отбрасыванием остатка (5)
+print(x%y) # остаток от деления (3)
