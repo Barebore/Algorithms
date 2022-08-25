47d7435b7d6ae39dc8997841de83971907c455ae
Barebore <borislavtarget@gmail.com>
2022-08-25 22:27:33 +0500

    123

--- Timofey_Khiryanov_lections/Lections/Lection_4.py
+++ Timofey_Khiryanov_lections/Lections/Lection_4.py
@@ -39,3 +39,18 @@ def is_simple_number(x):
 
 print(is_simple_number(142))
 
+def factorize_number(x):
+    ''' Раскладывает число на множители.
+        Печатает их на экран.
+        x - целое положительное число.
+    '''
+    divisor = 2
+    while x > 1:
+        if x%divisor == 0:
+            print(divisor)
+            x //= divisor
+        else:
+            divisor += 1
+
+
+factorize_number(53439453452347)
