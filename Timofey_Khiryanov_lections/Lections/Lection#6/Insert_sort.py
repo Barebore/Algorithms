ebaa509968d970da99ef4102359fa8fe0dcb20f2
Barebore <borislavtarget@gmail.com>
2022-08-29 14:08:42 +0500

    123

--- Timofey_Khiryanov_lections/Lections/Lection#6/Insert_sort.py
+++ Timofey_Khiryanov_lections/Lections/Lection#6/Insert_sort.py
@@ -1,12 +1,15 @@
 A = [4,2,5,1,3]
-for x in range(1,len(A),1):
-    print('первый цикл № x=', x)
-    for k in range(x,0,-1):
-        print('k =',k,'сравниваемые элементы', A[x], A[k-1])
-        if A[x] < A[k-1]:
-            print('прошло')
-            A[k-1], A[x] = A[x], A[k-1]
-            x -= 1
-            print(A)
-
+def insert_sort(A):
+    '''Сортировка списка А вставками '''
+    for x in range(1,len(A),1):
+        print('первый цикл № x=', x)
+        for k in range(x,0,-1):
+            print('k =',k,'сравниваемые элементы', A[x], A[k-1])
+            if A[x] < A[k-1]:
+                print('прошло')
+                A[k-1], A[x] = A[x], A[k-1]
+                x -= 1
+                print(A)
+insert_sort(A)
 print(A)
+
--- Timofey_Khiryanov_lections/Lections/Lection#6/buble_sort.py
+++ Timofey_Khiryanov_lections/Lections/Lection#6/buble_sort.py
@@ -1,10 +1,32 @@
 A = [4,2,5,1,3]
+def buble_sort(A):
+    '''Сортировка списка А методом пузырька'''
+    for x in range(1,len(A)):
+        print('cycle #',x)
+        for k in range(len(A)-x):
+            print('cycle # k =',k)
+            if A[k] > A[k+1]:
+                A[k], A[k+1] = A[k+1], A[k]
 
-for x in range(1,len(A)):
-    print('cycle #',x)
-    for k in range(len(A)-x):
-        print('cycle # k =',k)
-        if A[k] > A[k+1]:
-            A[k], A[k+1] = A[k+1], A[k]
+def test_sort(sort_algorithm):
+    print('testcase #1: ', end='')
+    A = [4,2,5,1,3]
+    A_sorted = [1,2,3,4,5]
+    sort_algorithm(A)
+    print('ok' if A == A_sorted else 'fail') #тернарный оператор
+    
+    print('testcase #2: ', end='')
+    A = list(range(10,20)) + list(range(0,10))
+    A_sorted = list(range(20))
+    sort_algorithm(A)
+    print('ok' if A == A_sorted else 'fail') #тернарный оператор
 
+    print('testcase #3: ', end='')
+    A = [4,2,2,1,4]
+    A_sorted = [1,2,2,4,4]
+    sort_algorithm(A)
+    print('ok' if A == A_sorted else 'fail') #тернарный оператор
+    
+
+buble_sort(A)
 print(A)
