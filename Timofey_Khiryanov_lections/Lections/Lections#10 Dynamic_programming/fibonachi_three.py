c1b2cea8767c413283efc45f8464191ccfc76978
Barebore <borislavtarget@gmail.com>
2022-09-03 23:54:26 +0500

    123

--- Timofey_Khiryanov_lections/Lections/Lections#10 Dynamic_programming/fibonachi_three.py
+++ Timofey_Khiryanov_lections/Lections/Lections#10 Dynamic_programming/fibonachi_three.py
@@ -14,12 +14,4 @@ def fib(n):
         fib[i] = fib[i-1] + fib[i-2]
     return fib[n]
 
-def fib(n):
-    fib = [0,1] + [0]*(n-1)
-    for i in range(2, n+1):
-        fib[i] = fib[i-1] + fib[i-2]
-    return fib[n]
-
-print(fib(10))
-print(fib(10))
 print(fib(10))
