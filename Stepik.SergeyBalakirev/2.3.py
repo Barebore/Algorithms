f602084fe1610b0fa5f415307563af4783e85e96
Barebore <borislavtarget@gmail.com>
2022-09-14 16:35:07 +0500

    123

---
+++ Stepik.SergeyBalakirev/2.3.py
@@ -0,0 +1,15 @@
+c = []
+c = [i for i in range(1,10,1.1)]
+for i in c:
+    print(i,round(i))
+
+# ввод данных
+a, b = map(int, input().split())
+
+# здесь продолжите программу
+a = 2
+b = 3
+print(round((a**2+b**2)**0.5),2)
+
+
+
