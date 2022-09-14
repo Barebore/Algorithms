29b0917885b72a44dcddc427ba964be172bc459d
Barebore <borislavtarget@gmail.com>
2022-09-15 01:55:44 +0500

    123

---
+++ Stepik.SergeyBalakirev/3.3.py
@@ -0,0 +1,8 @@
+# put your python code here
+a = input()
+print(a[0].upper() + a[1:].lower())
+
+# put your python code here
+a = input()
+print(a.replace(' ', ';'))
+
---
+++ Stepik.SergeyBalakirev/3.4.py
@@ -0,0 +1,4 @@
+a = input()
+a = a.replace(' ','\"')
+с = a.find("\"")
+print(c)
