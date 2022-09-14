487947b7aa9419bafd0ebb6dab4d157e04a67a97
Barebore <borislavtarget@gmail.com>
2022-09-14 21:44:17 +0500

    123

---
+++ Stepik.SergeyBalakirev/3.1.py
@@ -0,0 +1,12 @@
+s1 = 'Панда'
+s2 = 'Panda'
+text = '''Z пайтон выучил бы только за то,
+что есть популярные курсы.
+Много хороших курсов'''
+
+print(s1)
+print(s2)
+
+# put your python code here
+a,b = map(int, input().split())
+print('Пемеренная а = ' + str(a) + ', переменная b = ' + str(b))
