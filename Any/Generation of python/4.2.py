13b77be9a13e4e61f4c854239650dd3f6bb8056d
Barebore <borislavtarget@gmail.com>
2022-10-20 16:22:31 +0200

    213

---
+++ Any/Generation of python/4.2.py
@@ -0,0 +1,11 @@
+list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+total = 0
+counter = 0
+for lst in list1:
+    for number in lst:
+        counter += 1
+        total += number
+print(total, counter)
+
+
+
