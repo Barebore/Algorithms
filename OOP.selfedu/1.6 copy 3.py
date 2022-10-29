d3547bfc78eff69fe348204009b27c1e5e7374c9
Barebore <borislavtarget@gmail.com>
2022-10-29 20:23:21 +0200

    123

--- OOP.selfedu/1.6 copy 3.py
+++ OOP.selfedu/1.6 copy 3.py
@@ -1,33 +1,13 @@
-TYPE_OS = 1 # 1 - Windows; 2 - Linux
-
-class DialogWindows:
-    name_class = "DialogWindows"
-
-
-class DialogLinux:
-    name_class = "DialogLinux"
-
-class Dialog:
+class Point:
     def __new__(cls, *args, **kwargs):
-        if TYPE_OS == 1:
-            obj = super().__new__(DialogWindows)
-            setattr(obj, 'name', args[0])
-            return obj
-        obj = super().__new__(DialogLinux)
-        setattr(obj, 'name', args[0])
+        obj = super().__new__(cls)
+        setattr(obj, 'x', args[0])
+        setattr(obj, 'y', args[1])
         return obj
 
-# здесь объявляйте класс Dialog
+    def clone(self):
+        return Point(self.x, self.y)
 
-TYPE_OS = 1
-dlg_1 = Dialog("123")
-TYPE_OS = 2
-dlg_2 = Dialog("1234")
-print(dlg_1.name, dlg_2.name)
 
-d1 = Dialog("12")
-d2 = Dialog("123")
-print(d1.name, d2.name)
-print(dlg_2.name)
-print(d1.name)
-print(d2.name)
+pt = Point(1,2)
+pt_clone = pt.clone()
