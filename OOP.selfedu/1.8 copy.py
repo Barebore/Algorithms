56bf496f48ab5b17fd6061b3aa991f35438a584a
Barebore <borislavtarget@gmail.com>
2022-10-30 19:04:30 +0100

    13

--- OOP.selfedu/1.8 copy.py
+++ OOP.selfedu/1.8 copy.py
@@ -6,18 +6,16 @@ class GenerationIP:
         return GenerationIP.ip
         
 class Server:
+    buffer = []
     def __init__(self):
         self.ip = GenerationIP.generatorIP()
-        self.buffer = []
 
     @staticmethod
     def send_data(data):
         Router.buffer.append(data)
     
     def get_data(self):
-        a = self.buffer
-        self.buffer = []
-        return a
+        return self.buffer
     
     def get_ip(self):
         return self.ip
@@ -25,31 +23,30 @@ class Server:
 class Router:
     buffer = []
     server = []
-    
-    def link(self, server):
-        self.server.append(server)
-    
-    def unlink(self, server):
-        del self.server[self.server.index(server)]
+    @classmethod
+    def link(cls, server):
+        cls.server.append(server)
+    @classmethod
+    def unlink(cls, server):
+        del cls.server[cls.server.index(server)]
         
-    def send_data(self):
-        for i in self.buffer:
-            for j in range(len(self.server)):
+    @classmethod
+    def send_data(cls):
+        for i in cls.buffer:
+            for j in range(len(cls.server)):
                # print(cls.server[j].get_ip, i.ip)
-                #print(j)
-                if self.server[j].ip == i.ip:
-                    #print(self.server[j],self.server[j].ip, i.ip, i.data, self.server[j].buffer)
-                    self.server[j].buffer.append(i)
-                    #print(self.server[j], self.server[j].buffer)
-                    #print(self.server[j].buffer)
-                    #cls.server[j].buffer = []          
-        self.buffer = []
+                if cls.server[j].ip == i.ip:
+                    #print(cls.server[j].get_ip, i.ip)
+                    cls.server[j].buffer = i.data
+                    #cls.server[j].buffer = []
+        cls.buffer = []
             
         
 class Data:
     def __init__(self, data, ip):
         self.data = data
         self.ip = ip
+
 
 assert hasattr(Router, 'link') and hasattr(Router, 'unlink') and hasattr(Router, 'send_data'), "в классе Router присутсвутю не все методы, указанные в задании"
 assert hasattr(Server, 'send_data') and hasattr(Server, 'get_data') and hasattr(Server, 'get_ip'), "в классе Server присутсвутю не все методы, указанные в задании"
@@ -68,6 +65,7 @@ sv_to.send_data(Data("Hi", sv_from.get_ip()))
 router.send_data()
 msg_lst_from = sv_from.get_data()
 msg_lst_to = sv_to.get_data()
+print(msg_lst_to)
 assert len(router.buffer) == 0, "после отправки сообщений буфер в роутере должен очищаться"
 assert len(sv_from.buffer) == 0, "после получения сообщений буфер сервера должен очищаться"
 assert len(msg_lst_to) == 2, "метод get_data вернул неверное число пакетов"
