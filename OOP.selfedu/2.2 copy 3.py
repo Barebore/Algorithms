044ac020b2cbf01c30c4fc1bb33031efcf73bb8b
Barebore <borislavtarget@gmail.com>
2022-11-07 12:02:36 +0100

    123

---
+++ OOP.selfedu/2.2 copy 3.py
@@ -0,0 +1,67 @@
+class StackObj:
+    def __init__(self, data):
+        self.__data = data
+        self.__next = None
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, data):
+        self.__data = data
+
+    @property
+    def next(self):
+        return self.__next
+    
+    @next.setter
+    def next(self, next):
+        #if next == None: #or isinstance(next, StackObj):
+        self.__next = next
+
+class Stack:
+
+    def __init__(self) -> None:
+        self.top = None
+
+    def push(self,obj):
+        if self.top is None:
+            self.top = obj
+        else:
+            next_temp = self.top
+            while not(next_temp.next is None):
+                next_temp = next_temp.next
+            next_temp.next = obj
+
+    def pop(self):
+        if self.top is None:
+            return
+        if self.top.next is None:
+            a = self.top
+            self.top = None
+            return a
+        next_temp = self.top
+        while not(next_temp.next.next is None):
+            next_temp = next_temp.next
+        a = next_temp.next
+        next_temp.next = None
+        return a
+
+    def get_data(self):
+        next_temp = self.top
+        lst = []
+        while next_temp:
+            lst.append(next_temp.data)
+            next_temp = next_temp.next
+        return lst
+
+
+st = Stack()
+st.push(StackObj("obj1"))
+st.push(StackObj("obj2"))
+b = StackObj("obj3")
+print(b)
+st.push(b)
+print(st.pop())
+res = st.get_data()    # ['obj1', 'obj2']
+print(res)
