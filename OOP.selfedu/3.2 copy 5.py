87cd36ee5247b876606ca33c8d619a5edc1c0b7a
Borislav Pecherskikh <borislavtarget@gmail.com>
2023-01-31 17:47:33 +0500

    1234

--- OOP.selfedu/3.2 copy 5.py
+++ OOP.selfedu/3.2 copy 5.py
@@ -1,12 +1,17 @@
 class HandlerGET:
-    def __init__(self, func) -> None:
-        self.__func = func
-    def __call__(self, get_dict):
-        pass
+    def __init__(self, func):
+        self.__fn = func
+
+    def __call__(self, *args, **kwargs):
+        return self.get(self.__fn, *args, **kwargs)
+
+    def get(self, func, request, *args, **kwargs):
+        if request['method']:
+            return f'{request["method"]}: {func(*args, **kwargs)}'
 
 @HandlerGET
 def contact(request):
     return "Сергей Балакирев"
 
-
-res = contact({'method': 'GET', 'url': 'contact.html'})
+res = contact({"method": "GET", "url": "contact.html"})
+print(res)
