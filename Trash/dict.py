745e328803c5e82e631e2e0570020611e91eedca
Борислав Печерских <borislavtarget@gmail.com>
2023-04-08 23:50:50 +0500

    123123

---
+++ Trash/dict.py
@@ -0,0 +1,6 @@
+dct1 = {1: 'one', 5: 'two'}
+dct2 = dct1
+dct1[1] = '111'
+
+print(dct2)
+print(dct1)
---
+++ Trash/pop.py
@@ -0,0 +1,7 @@
+a = [1,2,3]
+print(a.pop())
+print(a.pop(0))
+print(a)
+
+a.extend(4)
+print(a)
