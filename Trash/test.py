67b1a1317992c5c7cceafc6466292c86af250b4d
Борислав Печерских <borislavtarget@gmail.com>
2023-04-09 20:15:19 +0500

    13

---
+++ Trash/test.py
@@ -0,0 +1,15 @@
+def knit_paint():
+    input_code = input("Enter the code: ")
+    if input_code == "knit":
+        print("You knit a sweater.")    
+
+def main():
+    knit_paint()
+
+if __name__ == "__main__":
+    main()
+
+# Path: test.py                                 
+# Compare this snippet from pop.py:
+# a = [1,2,3]
+# print(a.pop())
