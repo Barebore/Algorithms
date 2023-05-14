98d488cf48ec4a60914b86cf790936b1962c26f0
Борислав Печерских <borislavtarget@gmail.com>
2023-05-15 00:48:07 +0500

    123

--- Yandex_Praktikum/Algosiki/sprint3/1.search_in_crash_array.py
+++ Yandex_Praktikum/Algosiki/sprint3/1.search_in_crash_array.py
@@ -1,30 +1,61 @@
-# 87258919
+# 87298172
+def find_shift(arr):
+    '''
+    Функция для поиска смещения в отсортированном и сдвинутом массиве.
+    
+    Параметры:
+    arr (list): Входной массив.
+    
+    Возвращает:
+    int: Индекс точки смещения.
+    '''
+    low, high = 0, len(arr) - 1
+    if arr[low] < arr[high]:
+        return 0
+    while low <= high:
+        mid = low + (high - low) // 2
+        if arr[mid] > arr[mid + 1]:
+            return mid + 1
+        else:
+            if arr[mid] < arr[low]:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+def binary_search(arr, target, start, end):
+    '''
+    Функция для выполнения бинарного поиска в массиве.
+    
+    Параметры:
+    arr (list): Входной массив.
+    target (int): Искомое значение.
+    start (int): Индекс начала поиска.
+    end (int): Индекс окончания поиска.
+    
+    Возвращает:
+    int: Индекс искомого элемента или -1, если элемент не найден.
+    '''
+    while start <= end:
+        mid = start + (end - start) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return -1
+
 def broken_search(nums, target):
-    def find_shift(arr):
-        low, high = 0, len(arr) - 1
-        if arr[low] < arr[high]:
-            return 0
-        while low <= high:
-            mid = low + (high - low) // 2
-            if arr[mid] > arr[mid + 1]:
-                return mid + 1
-            else:
-                if arr[mid] < arr[low]:
-                    high = mid - 1
-                else:
-                    low = mid + 1
-
-    def binary_search(arr, target, start, end):
-        while start <= end:
-            mid = start + (end - start) // 2
-            if arr[mid] == target:
-                return mid
-            elif arr[mid] < target:
-                start = mid + 1
-            else:
-                end = mid - 1
-        return -1
-
+    '''
+    Функция для поиска элемента в отсортированном и сдвинутом массиве.
+    
+    Параметры:
+    nums (list): Входной массив.
+    target (int): Искомое значение.
+    
+    Возвращает:
+    int: Индекс искомого элемента или -1, если элемент не найден.
+    '''
     length_arr = len(nums)
     if length_arr == 1:
         return 0 if target == nums[0] else -1
@@ -38,3 +69,6 @@ def broken_search(nums, target):
     if target < nums[0]:
         return binary_search(nums, target, shift, length_arr - 1)
     return binary_search(nums, target, 0, shift)
+
+if __name__ == "__main__":
+    pass
--- Yandex_Praktikum/Algosiki/sprint3/2.effective_fast_sort.py
+++ Yandex_Praktikum/Algosiki/sprint3/2.effective_fast_sort.py
@@ -1,11 +1,27 @@
-# 87261554
+# 87299335
 def quicksort(arr, low, high):
+    """
+    Реализует алгоритм быстрой сортировки.
+    
+    Параметры:
+    arr (list): список, который нужно отсортировать
+    low (int): индекс начала сортировки
+    high (int): индекс окончания сортировки
+    """
     if low < high:
         pivot_index = partition(arr, low, high)
         quicksort(arr, low, pivot_index - 1)
         quicksort(arr, pivot_index + 1, high)
 
 def partition(arr, low, high):
+    """
+    Выполняет разделение списка на подсписки в алгоритме быстрой сортировки.
+    
+    Параметры:
+    arr (list): список, который нужно разделить
+    low (int): индекс начала разделения
+    high (int): индекс окончания разделения
+    """
     pivot = arr[high]
     i = low - 1
 
@@ -18,30 +34,35 @@ def partition(arr, low, high):
     return i + 1
 
 def compare(a, b):
-    if a[1] > b[1]:
-        return True
-    elif a[1] < b[1]:
-        return False
-    else:
-        if a[2] < b[2]:
-            return True
-        elif a[2] > b[2]:
-            return False
-        else:
-            return a[0] < b[0]
+    """
+    Сравнивает два кортежа по определенным правилам.
+    
+    Параметры:
+    a (tuple): первый кортеж для сравнения
+    b (tuple): второй кортеж для сравнения
+    """
+    return (a[1]*-1, a[2], a[0]) < (b[1]*-1, b[2], b[0])
 
 def swap(arr, i, j):
+    """
+    Меняет местами два элемента списка.
+    
+    Параметры:
+    arr (list): список, в котором нужно поменять элементы местами
+    i (int): индекс первого элемента
+    j (int): индекс второго элемента
+    """
     arr[i], arr[j] = arr[j], arr[i]
 
+if __name__ == "__main__":
+    n = int(input())
+    participants = []
 
-n = int(input())
-participants = []
+    for _ in range(n):
+        login, solved, penalty = input().split()
+        participants.append((login, int(solved), int(penalty)))
 
-for _ in range(n):
-    login, solved, penalty = input().split()
-    participants.append((login, int(solved), int(penalty)))
+    quicksort(participants, 0, n - 1)
 
-quicksort(participants, 0, n - 1)
-
-for participant in participants:
-    print(participant[0])
+    for participant in participants:
+        print(participant[0])
