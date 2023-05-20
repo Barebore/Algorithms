97ba7949a3d912c80155bebc199d8a34974812ba
Борислав Печерских <borislavtarget@gmail.com>
2023-05-21 00:51:56 +0500

    13

--- Mentor/test.py
+++ Mentor/test.py
@@ -1,1 +1,28 @@
-print((-1) % 1000)
+class Gtlt:
+    def __gt__(self, other):
+        return True
+
+    def __lt__(self, other):
+        return False
+
+
+class Triangle(Gtlt):
+    pass
+    
+class Square(Gtlt):
+    pass
+
+class Other(Triangle, Square):
+    pass
+
+a = Triangle()
+b = Square()
+
+
+c = Other()
+
+print(int.__super__.__name__)
+print(c.mro)
+
+if a>b:
+    print("a is bigger")
