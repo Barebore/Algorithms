52a370952d5f87faeea6e7f655c5b55bdac97e9e
Борислав Печерских <borislavtarget@gmail.com>
2023-06-05 17:45:57 +0500

    123

--- OOP.selfedu/3.4 copy 7.py
+++ OOP.selfedu/3.4 copy 7.py
@@ -1,52 +1,73 @@
-class Box3D:
-    def __init__(self, width, height, depth):
-        self.width = width
-        self.height = height
-        self.depth = depth
-
-    def __add__(self, other):
-        if isinstance(other, Box3D):
-            return Box3D(
-                self.width + other.width,
-                self.height + other.height,
-                self.depth + other.depth
-            )
-        
-    def __mul__(self, other):
-        if isinstance(other, int):
-            return Box3D(self.width * other, self.height * other, self.depth * other)
-    
-    def __rmul__(self, other):
-        return self * other
-    
-    def __sub__(self, other):
-        return Box3D(
-            self.width - other.width,
-            self.height - other.height,
-            self.depth - other.depth
-        )
-    
-    def __floordiv__(self, other):
-        return Box3D(
-            self.width // other,
-            self.height // other,
-            self.depth // other
-        )
-    
-    def __mod__(self, other):
-        return Box3D(
-            self.width % other,
-            self.height % other,
-            self.depth % other
-        )
+# Подвиг 10 (на повторение). В нейронных сетях использую операцию под названием Max Pooling. Суть ее состоит в сканировании прямоугольной таблицы чисел (матрицы) окном определенного размера (обычно, 2x2 элемента) и выбора наибольшего значения в пределах этого окна:
 
 
-box1 = Box3D(1, 2, 3)
-box2 = Box3D(2, 4, 6)
 
-box = box1 + box2 # Box3D: width=3, height=6, depth=9 (соответствующие размерности складываются)
-box = box1 * 2    # Box3D: width=2, height=4, depth=6 (каждая размерность умножается на 2)
-box = 3 * box2    # Box3D: width=6, height=12, depth=18
-box = box2 - box1 # Box3D: width=1, height=2, depth=3 (соответствующие размерности вычитаются)
-box = box1 // 2   # Box3D: width=0, height=1, depth=1 (соответствующие размерности целочисленно делятся на 2)
-box = box2 % 3    # Box3D: width=2, height=1, depth=0
+#  Или, если окна выходят за пределы матрицы, то они пропускаются (игнорируются):
+
+
+
+# Мы повторим эту процедуру. Для этого в программе нужно объявить класс с именем MaxPooling, объекты которого создаются командой:
+
+# mp = MaxPooling(step=(2, 2), size=(2,2))
+# где step - шаг смещения окна по горизонтали и вертикали; size - размер окна по горизонтали и вертикали.
+
+# Параметры step и size по умолчанию должны принимать кортеж со значениями (2, 2).
+
+# Для выполнения операции Max Pooling используется команда:
+
+# res = mp(matrix)
+# где matrix - прямоугольная таблица чисел; res - ссылка на результат обработки таблицы matrix (должна создаваться новая таблица чисел.
+
+# Прямоугольную таблицу чисел следует описывать вложенными списками. Если при сканировании таблицы часть окна выходит за ее пределы, то эти данные отбрасывать (не учитывать все окно).
+
+# Если matrix не является прямоугольной таблицей или содержит хотя бы одно не числовое значение, то должно генерироваться исключение командой:
+
+# raise ValueError("Неверный формат для первого параметра matrix.")
+# Пример использования класса (эти строчки в программе писать не нужно):
+
+# mp = MaxPooling(step=(2, 2), size=(2,2))
+# res = mp([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])    # [[6, 8], [9, 7]]
+# Результатом будет таблица чисел:
+
+# 6 8
+# 9 7
+
+class MaxPooling:
+    def __init__(self, step=(2, 2), size=(2, 2)):
+        self.step = step
+        self.size = size
+
+    def __call__(self, matrix):
+        if not self._is_valid_matrix(matrix):
+            raise ValueError("Неверный формат для первого параметра matrix.")
+
+        pooled_matrix = []
+        for i in range(0, len(matrix), self.step[0]):
+            row = []
+            for j in range(0, len(matrix[0]), self.step[1]):
+                max_value = self._get_max_value(matrix, i, j)
+                if max_value is not None:
+                    row.append(max_value)
+            if row:
+                pooled_matrix.append(row)
+
+        return pooled_matrix
+
+    def _is_valid_matrix(self, matrix):
+        if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+            return False
+        for row in matrix:
+            if not all(isinstance(element, (int, float)) for element in row):
+                return False
+        return True
+
+    def _get_max_value(self, matrix, i, j):
+        values = []
+        for x in range(i, min(i+self.size[0], len(matrix))):
+            for y in range(j, min(j+self.size[1], len(matrix[0]))):
+                values.append(matrix[x][y])
+        return max(values) if values else None
+
+mp = MaxPooling(step=(2, 2), size=(2,2))
+res = mp([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])    # res =  [[6, 8], [9, 7]]
+print(res)
--- OOP.selfedu/3.5.py
+++ OOP.selfedu/3.5.py
@@ -1,11 +1,40 @@
 class Track:
-    def __init__(self, start_x, start_y):
+    def __init__(self, start_x=0, start_y=0):
+        self.tr = []
         self.start_x = start_x
         self.start_y = start_y
+
+    def add_track(self,tr):
+        self.tr.append(tr)
+
+    def get_tracks(self):   
+        return tuple(self.tr)
+    
+    def __len__(self):
+        total_length_track = 0
+        start_coords = [self.start_x, self.start_y]
+        for point in self.tr:
+            total_length_track += ((point.to_x - start_coords[0])**2 + (point.to_y - start_coords[1])**2)**0.5
+            start_coords = [point.to_x, point.to_y]
+
+        return int(total_length_track)
+    
+    def __eq__(self, other):
+        return len(self) == len(other)
+    
+    def __lt__(self, other):
+        return len(self) < len(other)
+
 
 class TrackLine:
     def __init__(self, to_x, to_y, max_speed):
         self.to_x = to_x
         self.to_y = to_y
         self.max_speed = max_speed
-        
+
+track1, track2 = Track(), Track(0, 1)
+track1.add_track(TrackLine(2, 4, 100))
+track1.add_track(TrackLine(5, -4, 100))
+track2.add_track(TrackLine(3, 2, 90))
+track2.add_track(TrackLine(10, 8, 90))
+res_eq = track1 == track2
